85e8c362ee89e1962a40c421b0f38c3b8bd29d97
Ahmed Sameh <ahmedelsherefy@gmail.com>
2023-05-18 17:32:33 +0300

    Update modelController.py

--- modelController.py
+++ modelController.py
@@ -134,17 +134,113 @@ def autocorrect(prediction):
     word = word.upper()
     return word
 
+def processOutsideImage(image):
+    mask = np.zeros(image.shape, dtype=np.uint8)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3,3), 0)
+    thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,51,9)
+
+    # Create horizontal kernel then dilate to connect text contours
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+    dilate = cv2.dilate(thresh, kernel, iterations=2)
+
+    # Find contours and filter out noise using contour approximation and area filtering
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+        x, y, w, h = cv2.boundingRect(c)
+        area = w * h
+        ar = w / float(h)
+        if area > 1200 and area < 50000 and ar < 8:
+            cv2.drawContours(mask, [c], -1, (255,255,255), -1)
+            # Crop the image based on the bounding box
+            cropped_image = image[y:y+h, x:x+w]
+            cv2.imwrite("result2.png", cropped_image)
+
+    # Bitwise-and input image and mask to get result
+    # mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    result = cv2.bitwise_and(image, image)
+    # result[mask==0] = (255,255,255) # Color background white
+
+    # NEW CODE HERE TO END _____________________________________________________________
+    gray2 = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+    thresh2 = cv2.threshold(gray2, 128, 255, cv2.THRESH_BINARY)[1]
+    thresh2 = 255 - thresh2
+    kernel = np.ones((5, 191), np.uint8)
+    close = cv2.morphologyEx(thresh2, cv2.MORPH_CLOSE, kernel)
+
+    # get external contours
+    contours = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+
+    # draw contours
+    result2 = result.copy()
+    for cntr in contours:
+        # get bounding boxes
+        pad = 100
+        x, y, w, h = cv2.boundingRect(cntr)
+        cv2.rectangle(result2, (x-pad, y-pad), (x+w+pad, y+h+pad), (0, 0, 255), 4)
+        
+
+    i = 0
+    for c in contours:
+        # get the bounding rect
+        x, y, w, h = cv2.boundingRect(c)
+        # to save the images
+        try:
+            cv2.imwrite('result2.png', image[y-50:y+h+50,x-50:x+w+50])
+        except:
+            continue
+        i += 1
+
+
+def brightenOutsideImage(img):
+    # increase contrast
+    pxmin = np.min(img)
+    pxmax = np.max(img)
+    imgContrast = (img - pxmin) / (pxmax - pxmin) * 400
+
+    # increase line width
+    kernel = np.ones((3, 3), np.uint8)
+    imgMorph = cv2.erode(imgContrast, kernel, iterations = 3)
+
+    return imgMorph
+
+
+
 def getPredictedWord():
     model = tf.keras.models.load_model('new_model.h5')
 
-    image = Image.open('captured_snapshot.jpg')
+    image = Image.open('ABCD.png')
     
     image = cropImage(image)
     
-    image = cv2.imread('result2.png', cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread('ABCD.png', cv2.IMREAD_GRAYSCALE)
     image = preprocessImage(image)
     
     
+    image = image/255.
+    pred = model.predict(image.reshape(1, 256, 64, 1))
+
+    decoded = K.get_value(K.ctc_decode(pred, input_length=np.ones(pred.shape[0])*pred.shape[1], 
+                                       greedy=True)[0][0])
+
+    predicted_word = autocorrect(num_to_label(decoded[0]))
+    
+    print("The Model predicted: " +num_to_label(decoded[0]))
+    print("After autocorrection: " +predicted_word)
+    return predicted_word
+
+def getPredictedOutsideWord():
+    model = tf.keras.models.load_model('new_model.h5')
+    image = cv2.imread("captured_snapshot_out.jpg")
+    image = processOutsideImage(image)
+    img = cv2.imread('result2.png', cv2.IMREAD_GRAYSCALE)
+    image = brightenOutsideImage(img)
+
     image = image/255.
     pred = model.predict(image.reshape(1, 256, 64, 1))
 
